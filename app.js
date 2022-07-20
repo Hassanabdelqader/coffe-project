ac13589577977303aeb21c604642f3a6e1e4c064
Hassanabdelqader <terawihassan@gmail.com>
2022-07-20 03:09:40 +0300

    update

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ Drink.prototype.renderTable = function () {
 
 let latte = new Drink("Latte", ["milk", "ice", "sugar"], "./assets/latte.png", true, true, 1);
 let mocha = new Drink("mocha", ["milk", "coffee", "ice", "sugar"], "./assets/mocha.png", true, false, 2);
-let hotChocalte = new Drink("hot chocalte", ["milk", "tea", "ice", "sugar"], "./assets/mocha.png", true, false, 2)
+let hotChocalte = new Drink("hot chocalte", ["milk", "coffee", "ice", "sugar"], "./assets/mocha.png", true, false, 3)
 
 function renderAll() {
     for (let i = 0; i < allDrinks.length; i++) {
@@ -89,7 +89,7 @@ function renderAll() {
 }
 
 let localdebug  = 0;
-renderAll();
+//renderAll();
 
 // local storage:
 function saveData(data) {
@@ -99,18 +99,22 @@ function saveData(data) {
 }
 
 
-function getData() {
-    let arrayData = JSON.parse(retrievedData);
-    // each object doesn't has access to render method
-    if (arrayData != null) {
-        for (let i = allDrinks.length; i < arrayData.length; i++) {
-            // reinstantiation: re creating instance
-            new Drink(arrayData[i].name, arrayData[i].ingredients, arrayData[i].image, arrayData[i].isCol, arrayData[i].isHot, arrayData[i].price);
+
+    function getData() {
+        let retrievedData = localStorage.getItem("drink");
+        
+        let arrayData = JSON.parse(retrievedData);
+        // each object doesn't has access to render method
+        if (arrayData != null) {
+            for (let i = 1; i < arrayData.length; i++) {
+                // reinstantiation: re creating instance
+                new Drink(arrayData[i].name, arrayData[i].ingredients, arrayData[i].image, arrayData[i].isCol, arrayData[i].isHot, arrayData[i].price);
+            }
         }
+        renderAll();
     }
-    renderAll();
-}
-getData();
+    
+    getData();
 
 
 
